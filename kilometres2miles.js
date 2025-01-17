0a55e2bbf378f0cc4dcfa6dd5209f2a7ca5fe9d4
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-01-17 01:15:44 +0000

    add in2ft and cylinder volume programs

---
+++ kilometres2miles.js
@@ -0,0 +1,7 @@
+// This program *converts* **kilometres** to **miles**
+
+let kilometres = 1;
+
+let miles = kilometres/1.609344;
+
+console.log(`${kilometres} kilometres is ${miles} miles`);
---
+++ volumeOfCylinder.js
@@ -0,0 +1,14 @@
+// This program *calculates* the **volume of a cylinder** 
+// based on the **diameter** of the end of the circle
+// and the **height** of the cylinder
+
+
+let diameter =1;
+
+let height = 2;
+
+let volume = Math.PI*(diameter/2)**2*height;
+
+console.log(`${volume} is the volume of the cylinder`);
+
+
