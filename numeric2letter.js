24ff17c751fa01d864741d9a3665dcc6439e1c3c
LianaV27 <144968889+LianaV27@users.noreply.github.com>
2025-01-24 00:37:25 +0000

    add message to display current numeric grade

--- numeric2letter.js
+++ numeric2letter.js
@@ -1,38 +1,38 @@
 // This program *calculates* a **letter grade** based on a *numeric grade**
 
 // assign numeric grade
-let numericGrade = 100;
+let numericGrade = 99;
 
 
 // assign letter grades to numeric values
 
 if (numericGrade >= 90){
-    console.log(`letter grade is A+`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade A+`);
 
 } else if (numericGrade >= 85 && numericGrade <=89){
-    console.log(`letter grade is A`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade A`);
 
 } else if (numericGrade >= 80 && numericGrade <=84){
-    console.log(`letter grade is A-`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade A-`);
 
 } else if (numericGrade >= 77 && numericGrade <=79){
-    console.log(`letter grade is B+`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade B+`);
 
 } else if (numericGrade >= 73 && numericGrade <=76){
-    console.log(`letter grade is B`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade B`);
 
 } else if (numericGrade >= 70 && numericGrade <=72){
-    console.log(`letter grade is B-`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade B-`);
 
 } else if (numericGrade >= 67 && numericGrade <=69){
-    console.log(`letter grade is C+`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade C+`);
 
 } else if (numericGrade >= 60 && numericGrade <=66){
-    console.log(`letter grade is C`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade C`);
 
 }else if (numericGrade <60 && numericGrade>=51){
-    console.log(`letter grade is D`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade D`);
 
 }else if (numericGrade <=50){
-    console.log(`letter grade is F`);
+    console.log(`a numeric grade of ${numericGrade} is letter grade F`);
 }
